bba5cc80c4bbecabd97d79bfdcb148d44b404960
Raunakkhatun <raunakkhatun45655@gmail.com>
2025-04-23 20:29:23 -0700

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout << "enter a number";
+    cin >> number;
+    if(number > 0){
+    cout << "number is positive";
+    }
+    else{
+        cout <<"number is negative";
+    }
+    return 0;
+
+}
