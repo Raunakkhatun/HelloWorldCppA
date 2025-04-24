b4f4cc81bf6d012af28d0e2e53ea289543ce94b2
Raunakkhatun <raunakkhatun45655@gmail.com>
2025-04-23 20:28:57 -0700

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout << "enter an integer";
+    cin >> x;
+    if(x % 2 == 0){
+        cout << "x is even";
+    }
+    else{
+        cout << "x ix odd";
+    }
+
+    return 0;
+
+}
